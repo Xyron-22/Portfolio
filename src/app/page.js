afdfd20a55454fd615fc46a3d980788893b05dd4
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2023-10-11 16:59:02 +0800

    2nd children bug fixed

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     <div className='h-[82vh] md:h-[75vh] lg:h-[68vh] container-fluid text-center overflow-auto flex justify-center items-center flex-col lg:flex-row'>
       <div className='flex justify-center items-center lg:h-[20%] flex-col transition-all duration-700 ease-in-out'>
       <h1 className='font-anton font-bold text-2xl md:text-5xl tracking-wide text-gray'>Hello. World.</h1>
-      <h1 className='font-anton font-bold text-5xl md:text-7xl lg:text-8xl tracking-wide'>I'm Xyron Uy</h1>
+      <h1 className='font-anton font-bold text-5xl md:text-7xl lg:text-8xl tracking-wide'>I&apos;m Xyron Uy</h1>
       <h1 className='font-anton font-bold text-2xl md:text-5xl tracking-wide'>A Fullstack Developer</h1>
       </div>
       <div>
